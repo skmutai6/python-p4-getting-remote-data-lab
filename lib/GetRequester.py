d2a3da51dafbae980962cf3eceb89595f65a34bb
Shadrack <skmutai6@gmail.com>
2024-07-02 21:45:16 +0300

    commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,9 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
-    def get_response_body(self):
-        pass
-
+    def get_response_body(self):        
+        response = requests.get('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+        return response.content
+ 
     def load_json(self):
         pass
